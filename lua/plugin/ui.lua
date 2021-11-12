2070328a9d8e7feeffbcf0d83871dfc6cafdedb2
Evolution404 <615598813@qq.com>
2021-11-12 08:56:50 +0800

    配置gitsigns

--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -32,7 +32,25 @@ return function(use)
         },
         sections = {
           lualine_a = {{'mode', fmt = function(str) return str:sub(1,1) end}},
-          lualine_b = {{'branch'}, {'diff'}},
+          lualine_b = {
+            {'branch'},
+            {
+              'diff',
+              colored = true,
+              diff_color = {
+                added = {fg = "#A0C980"},
+                modified = {fg = "#6CB6EB"},
+                removed = {fg = "#EC7279"},
+              },
+              -- 利用gitsigns插件来提供文件变动的信息
+              source = function()
+                local dict = vim.b.gitsigns_status_dict
+                if(dict) then
+                  return {added = dict.added, modified = dict.changed, removed = dict.removed}
+                end
+                return dict
+              end
+            }},
           lualine_c = {
             {'filename', path = 1}, {gps.get_location, condition = gps.is_available}, {'lsp_progress'}
           },
@@ -174,44 +192,36 @@ return function(use)
       end
       require('gitsigns').setup {
         signs = {
-          add = {hl = 'GitGutterAdd', text = '▋'},
-          change = {hl = 'GitGutterChange', text = '▋'},
-          delete = {hl = 'GitGutterDelete', text = '▋'},
-          topdelete = {hl = 'GitGutterDeleteChange', text = '▔'},
-          changedelete = {hl = 'GitGutterChange', text = '▎'}
+          add = {text = '▋'},
+          change = {text = '▋'},
+          delete = {text = '▋'},
+          topdelete = {text = '‾'},
+          changedelete = {text = '~'},
         },
         keymaps = {
-          -- Default keymap options
+          -- 以下定义了一系列按键操作
           noremap = true,
           buffer = true,
-          ['n ]g'] = {
-            expr = true,
-            "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"
-          },
-          ['n [g'] = {
-            expr = true,
-            "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"
-          },
+          -- 在hunk之间前后跳转
+          ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
+          ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
+          -- 保存或撤销暂存区
           ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
           ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
           ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+          -- 将编辑内容恢复到git暂存区内容，可以恢复一个hunk或者恢复整个文件
           ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
           ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
           ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+          -- 对比当前hunk所做的修改
           ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+          -- 查看产生当前行的提交
           ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-          -- Text objects
-          ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-          ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+          -- 定义文本对象
+          ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+          ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
         },
-        watch_gitdir = {interval = 1000, follow_files = true},
-        current_line_blame = true,
-        current_line_blame_opts = {delay = 1000, virtual_text_pos = 'eol'},
-        sign_priority = 6,
-        update_debounce = 100,
-        status_formatter = nil, -- Use default
-        word_diff = false,
-        diff_opts = {internal = true}
+        diff_opts = {algorithm = "minimal"}
       }
     end
   }
