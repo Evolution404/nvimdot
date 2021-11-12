599fb5ce407caaaacbe762d13c0a5af2ee5a7420
Evolution404 <615598813@qq.com>
2021-11-12 09:43:16 +0800

    lualine去掉不必要的大括号

--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -33,7 +33,7 @@ return function(use)
         sections = {
           lualine_a = {{'mode', fmt = function(str) return str:sub(1,1) end}},
           lualine_b = {
-            {'branch'},
+            'branch',
             {
               'diff',
               colored = true,
