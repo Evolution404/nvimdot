29b705f6233d625a7fcb9b1694a890b965c90959
Evolution404 <615598813@qq.com>
2021-11-27 22:41:29 +0800

    配置nvim-treesitter

--- lua/plugin/editor.lua
+++ lua/plugin/editor.lua
@@ -1,6 +1,6 @@
 local set_keymap = vim.api.nvim_set_keymap
 return function(use)
-  -- 大纲插件
+	-- 大纲插件
 	set_keymap("n", "<leader>l", ":SymbolsOutline<cr>", { noremap = true, silent = true })
 	use({
 		"simrat39/symbols-outline.nvim",
@@ -47,46 +47,17 @@ return function(use)
 
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = "maintained",
-				highlight = { enable = true, disable = { "vim" } },
-				textobjects = {
-					select = {
-						enable = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]["] = "@function.outer",
-							["]m"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]]"] = "@function.outer",
-							["]M"] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[["] = "@function.outer",
-							["[m"] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[]"] = "@function.outer",
-							["[M"] = "@class.outer",
-						},
-					},
-				},
-				rainbow = {
+				highlight = { enable = true },
+				incremental_selection = {
 					enable = true,
-					extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-					max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+					keymaps = {
+						init_selection = "sv",
+						node_incremental = "n",
+						scope_incremental = "v",
+						node_decremental = "b",
+					},
 				},
-				context_commentstring = { enable = true, enable_autocmd = false },
-				matchup = { enable = true },
-				context = { enable = true, throttle = true },
+				textobjects = { enable = true },
 			})
 		end,
 	})
